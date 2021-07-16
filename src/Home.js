7009956143db7ad470db0a7b33881ff943e7b883
matheustakasaki <matheus8antunes@gmail.com>
2021-07-16 19:32:37 -0300

    Routes

---
+++ src/Home.js
@@ -0,0 +1,13 @@
+
+
+
+
+
+//   const [usuario, setUsuario] = useState('')
+
+//   const handlePesquisa = () => {
+//     axios.get(`https://api.github.com/users/${usuario}/repos`).then(response => console.log(response))
+//   }
+//   <h1> {usuario}</h1>
+//   <input className="usuarioInput" placeholder="Usuário" value={usuario} onChange={e => setUsuario(e.target.value)}></input>
+//   <button type="button" onClick={handlePesquisa}>Pesquisar</button>
---
+++ src/routes.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Switch, Route, BrowserRouter } from 'react-router-dom'
+import Home from './Home'
+import Repositories from './Repositories';
+
+export default function Routes() {
+    return (
+        <BrowserRouter>
+            <Switch>
+                <Route path='/' exact component={Home}></Route>
+                <Route path='/repositories' component={Repositories}></Route>
+            </Switch>
+
+        </BrowserRouter>
+    )
+}
