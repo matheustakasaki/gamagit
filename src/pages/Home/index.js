04c4687a9e855b9a4f5b9e115e756ac66398f6c3
matheustakasaki <matheus8antunes@gmail.com>
2021-07-19 23:57:44 -0300

    Salvando dados no Local Storage

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -6,8 +6,17 @@ export default function App(props) {
 
     const [usuario, setUsuario] = useState('')
 
-    const handlePesquisa = () => {
-        axios.get(`https://api.github.com/users/${usuario}/repos`).then(response => console.log(response))
+    function handlePesquisa() {
+        axios.get(`https://api.github.com/users/${usuario}/repos`).then(response => {
+            const repositories = response.data;
+            const repositoriesName = [];
+
+            repositories.map((repository) => {
+                repositoriesName.push(repository.name)
+            });
+            localStorage.setItem('repositoriesName', JSON.stringify(repositoriesName))
+
+        })
     }
 
     return (
