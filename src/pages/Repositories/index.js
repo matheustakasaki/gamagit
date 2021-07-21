5582c62a1606abe0b8b292a927d6ecfa2bcf989f
matheustakasaki <matheus8antunes@gmail.com>
2021-07-20 21:33:39 -0300

    Renderizando repositórios e programação declarativa

--- src/pages/Repositories/index.js
+++ src/pages/Repositories/index.js
@@ -1,19 +1,26 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import * as S from './styled'
 export default function Repositories() {
 
+
+    const [ repositories, setRepositories ] = useState([]);
+
+    useEffect( () => {
+        let repositoriesName = localStorage.getItem('repositoriesName');
+        repositoriesName  = JSON.parse(repositoriesName);
+        setRepositories(repositoriesName);
+        localStorage.clear();
+    }, []);
+    
     return (
         <>
             <S.Title>Repositories</S.Title>
             <S.List>
-                <S.ListItem>Repositório 1</S.ListItem>
-                <S.ListItem>Repositório 1</S.ListItem>
-                <S.ListItem>Repositório 1</S.ListItem>
-                <S.ListItem>Repositório 1</S.ListItem>
-                <S.ListItem>Repositório 1</S.ListItem>
-                <S.ListItem>Repositório 1</S.ListItem>
-                <S.ListItem>Repositório 1</S.ListItem>
-                <S.ListItem>Repositório 1</S.ListItem>
+                { repositories.map( repository => {
+                    return(
+                        <S.ListItem> Repositório: { repository } </S.ListItem>
+                    )
+                })}
             </S.List>
 
         </>
